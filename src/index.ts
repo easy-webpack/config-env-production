7904c68c2d9e34ab596eebaf6868e41657d2f826
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-11-08 18:01:18 +0100

    fix(index): remove deprecated debug

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,6 @@ export = function production({devtool = 'source-map', dedupe = true, htmlLoaderO
 
   return function production(this: WebpackConfigWithMetadata): WebpackConfigWithMetadata {
     const config = {
-      debug: false,
       devtool,
       /**
        * Options affecting the output of the compilation.
